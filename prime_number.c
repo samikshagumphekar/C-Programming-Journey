a9128ab018901ef074d51977a437fe2a8df68a74
Samiksha Gumphekar <samikshagumphekar17@gmail.com>
2026-02-25 18:52:58 +0530

    Added working prime number program

---
+++ prime_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    flag = 0;
+    for(i = 2; i <= n/2; ++i) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (n == 1) {
+        printf("1 is neither prime nor composite.\n");
+    }
+    else {
+        if (flag == 0)
+            printf("%d is a prime number.\n", n);
+        else
+            printf("%d is not a prime number.\n", n);
+    }
+
+    return 0;
+}
